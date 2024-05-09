58e7dbee0bda62c72819f39e860d1efb7da0ddee
ddolboghi <rhdfyd128@gmail.com>
2024-05-09 22:31:33 +0900

    feat: 서비스 간 통신 위한 annotation 추가

--- src/main/java/com/leeforgiveness/memberservice/MemberserviceApplication.java
+++ src/main/java/com/leeforgiveness/memberservice/MemberserviceApplication.java
@@ -2,14 +2,15 @@ package com.leeforgiveness.memberservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@RefreshScope
+@EnableDiscoveryClient
 public class MemberserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MemberserviceApplication.class, args);
 	}
-
 }
