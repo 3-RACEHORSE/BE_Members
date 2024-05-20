fe82c14e99bd9587150b8e868cc25d8622ce3db0
ddolboghi <rhdfyd128@gmail.com>
2024-05-20 21:19:59 +0900

    feat: jpa 쿼리 메서드 추가

--- src/main/java/com/leeforgiveness/memberservice/auth/infrastructure/MemberRepository.java
+++ src/main/java/com/leeforgiveness/memberservice/auth/infrastructure/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.leeforgiveness.memberservice.auth.infrastructure;
 
 import com.leeforgiveness.memberservice.auth.domain.Member;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -13,6 +14,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUuid(String uuid);
 
     Optional<Member> findByHandle(String handle);
+
+    List<Member> findByUuidIn(List<String> uuids);
 
     default Member findByUuidOrThrow(String uuid) {
         return findByUuid(uuid).orElseThrow(() -> new IllegalArgumentException("회원을 찾을 수 없습니다."));
