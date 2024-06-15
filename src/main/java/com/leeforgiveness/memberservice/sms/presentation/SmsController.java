2ba926631320277381dccbf42b03f6b126bb8572
ddolboghi <rhdfyd128@gmail.com>
2024-06-15 17:19:51 +0900

    refactor: sms 인증 API CORS 설정

--- src/main/java/com/leeforgiveness/memberservice/sms/presentation/SmsController.java
+++ src/main/java/com/leeforgiveness/memberservice/sms/presentation/SmsController.java
@@ -8,11 +8,13 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(value = "*")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
