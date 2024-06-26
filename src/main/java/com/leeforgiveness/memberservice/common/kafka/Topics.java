45da78831be3412c6bd84fe6d7f3053d6af0208b
hyun7494 <et009153@gmail.com>
2024-06-26 17:28:48 +0900

    feat: 채팅 서비스로 변경사항 전송

--- src/main/java/com/leeforgiveness/memberservice/common/kafka/Topics.java
+++ src/main/java/com/leeforgiveness/memberservice/common/kafka/Topics.java
@@ -10,8 +10,11 @@ public enum Topics {
     SEND_TO_CHAT(Constant.SEND_TO_CHAT);
 
     public static class Constant {
+
         public static final String SEND_TO_CHAT = "send-to-chat-topic";
         public static final String MEMBER_SERVICE = "alarm-topic";
+        public static final String CHANGE_PROFILE_IMAGE = "change-profile-image-topic";
     }
+
     private final String topic;
 }
